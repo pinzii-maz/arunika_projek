c61e6b936b1c85dbb1d2061da924725754a07162
rujakpolnes <rujakpolnes@gmail.com>
2024-11-10 14:21:25 +0800

    mencoba push

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda RUJAK</title>
+    <title>BERANDA RUJAK</title>
     <link rel="stylesheet" href="assets/css/styles.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
